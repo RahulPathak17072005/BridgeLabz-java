8eb5b116a31624132612725f9a9abe4101fac9fd
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-03-19 12:57:52 +0530

    first program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Day 1 - Hello World Program
+// Author: Rahul Pathak
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Rahul");
+    }
+}
