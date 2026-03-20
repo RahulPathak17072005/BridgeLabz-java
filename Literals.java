5007d258420bbf6f9cbf3aa9d06e59c0c895238a
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-03-20 11:45:05 +0530

    Literals and DataType

---
+++ Literals.java
@@ -0,0 +1,27 @@
+//Literal (General): A literal is a fixed value assigned directly to a variable in a program
+
+
+class BasicsDemo {
+    public static void main(String[] args) {
+        int decimal = 10;        // Decimal literal
+        int binary = 0b1010;     // Binary literal
+        int octal = 012;         // Octal literal
+        int hex = 0xA;           // Hexadecimal literal
+
+        float f = 5.5f;          // Float literal
+        double d = 10.25;        // Double literal
+        char ch = 'A';           // Character literal
+        String str = "Rahul";    // String literal
+        boolean flag = true;     // Boolean literal
+
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binary);
+        System.out.println("Octal: " + octal);
+        System.out.println("Hex: " + hex);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + ch);
+        System.out.println("String: " + str);
+        System.out.println("Boolean: " + flag);
+    }
+    }
---
+++ PrimitiveDataType.java
@@ -0,0 +1,27 @@
+//Primitive data types are the basic built-in data types in Java used to store 
+// simple values directly in memory.
+
+class PrimitiveDemo {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 100;
+        int a = 1000;
+        long l = 100000L;
+        float f = 5.5f;
+        double d = 10.25;
+        char c = 'A';
+        boolean flag = true;
+        
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + a);
+        System.out.println("Long: " + l);
+
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + flag);
+    }
+}
